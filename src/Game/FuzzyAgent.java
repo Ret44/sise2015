29bafc2b76ef4ca4f74e4350a5183e530e9d96f8
romanchom <romanchom@gmail.com>
2015-05-28 20:41:32 +0200

    Agent niewyraźny mnie pokonał.

--- src/Game/FuzzyAgent.java
+++ src/Game/FuzzyAgent.java
@@ -13,21 +13,27 @@ public class FuzzyAgent implements Agent {
 	public int decide(Choice[] choices, AgentStruct state) {
 		FIS fis = FIS.load(filename, true);
 
-		
-		// TODO add inputs
-		fis.setVariable("service", 3);
-		fis.setVariable("food", 7);
-		
-		fis.evaluate();
-		
-		
-		// TODO take outputs and return decision
-		fis.getVariable("tip").getLatestDefuzzifiedValue();
-		
+		double max = 0;
+		int index = 0;
 
-
-		JFuzzyChart.get().chart(fis.getFunctionBlock("tipper"));
-		return 0;
+		for(int i = 0; i < choices.length; ++i){
+			fis.setVariable("action", (double) choices[i].ordinal());
+			if(!state.choiceHistory.isEmpty()) 
+				fis.setVariable("previousAction", (double) state.choiceHistory.peek().ordinal());
+			fis.setVariable("power", ((double) state.item) / 10.0);
+			
+			fis.evaluate();
+			
+			double val = fis.getVariable("idea").getLatestDefuzzifiedValue();
+			if(val > max){
+				max = val;
+				index = i;
+			}
+		}
+		
+		System.out.println("AI choice: " + choices[index]);
+		//JFuzzyChart.get().chart(fis.getFunctionBlock("tipper"));
+		return index;
 	}
 
 	@Override
