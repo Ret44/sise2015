21285867ee58de3b836d765a63406ab087432baf
GC08 <gniewekciolek@gmail.com>
2015-06-18 18:06:09 +0200

    wlasny agent

--- src/Game/main.java
+++ src/Game/main.java
@@ -19,9 +19,9 @@ public class main {
 		arena.AddAgent(new CLIPSAgent("RomanChomik.clp"));
 		//arena.AddAgent(new FuzzyAgent("agent_bs.flc"));
 		arena.AddAgent(new CLIPSAgent("agent_bs.CLP"));
-		//arena.AddAgent(new CLIPSAgent("GniewomirCiolek.CLP"));
+		arena.AddAgent(new CLIPSAgent("GniewomirCiolek.CLP"));
 		
-		arena.Fight(200);
+		arena.Fight(2);
 		arena.PrintResults();//*/
 	}
 	
