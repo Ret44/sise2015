b83fc11fae42a65bd44887495f2b1ade85ae9aee
Bartłomiej Sieczka <sieczka.bartlomiej@gmail.com>
2015-06-19 02:00:48 +0200

    FuzzyAgent

--- src/Game/main.java
+++ src/Game/main.java
@@ -17,7 +17,7 @@ public class main {
 		Arena arena = new Arena();
 		arena.AddAgent(new FuzzyAgent("RomanChomik.flc"));
 		arena.AddAgent(new CLIPSAgent("RomanChomik.clp"));
-		//arena.AddAgent(new FuzzyAgent("agent_bs.flc"));
+		arena.AddAgent(new FuzzyAgent("BartlomiejSieczka.flc"));
 		arena.AddAgent(new CLIPSAgent("agent_bs.CLP"));
 		arena.AddAgent(new CLIPSAgent("GniewomirCiolek.CLP"));
 		
